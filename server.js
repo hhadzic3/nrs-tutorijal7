bf86a42617bf36f0e1e0f6ce677e496193a602e8
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-05-07 16:20:59 +0200

    Tutorijal 8

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var express = require('express');
-
 var app = express();
-
 app.use(express.static('.'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/gradovi', (req, res) => db.gradovi.findAll().then(gradovi => res.json(gradovi)));
+app.get('/gradovi/:id' , (req, res) =>  db.gradovi.findOne({
+    where: {   id: req.params.id }}).then( data => { res.send(data) })   
+);
 
+app.delete('/gradovi/:id' , (req, res) => db.gradovi.destroy({
+    where: { id: req.params.id }}).then( () => { res.json({ status : 'Deleted!'}) })  
+);
+app.post('/grad' , function(req, res)  {
+    if ( !req.body)
+        res.json({ error: 'Bad Data' })
+    db.gradovi.create(req.body)
+    .then( data => { res.send(data) }).catch( function (err) {res.sendStatus(500)});
+});
+
+app.put('/grad/:id' , function(req, res)  {
+    if ( !req.body )
+        res.json({ error: 'Bad Data' })
+    var v = req.body;
+    db.gradovi.update({
+        naziv: v.naziv ,
+        broj_stanovnika:v.broj_stanovnika,}, { where: { id: req.params.id } }
+    ).then( () => { res.json({ status : 'Updated!'}) });
+});
 
 app.listen('8080' , () => {console.log('Listening port 8080...')});
